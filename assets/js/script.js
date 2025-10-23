e9f1949032d2a8a78f34cd49cad3dca7f7faefd1
angelostori <angelostori13@gmail.com>
2025-10-23 16:41:40 +0200

    implement the logic to append the ten emails generate in the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,14 +12,21 @@ Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le a
 
 */
 const randomMail = 'https://flynn.boolean.careers/exercises/api/random/mail'
+const emailList = document.getElementById('email-list')
+
 
 for (let i = 0; i < 10; i++) {
     //chiamata axios
     axios.get(randomMail)
         .then((response) => {
-            console.log(response.data.response);
+            //catturo la mail dall'oggetto response
             const email = response.data.response
-
+            //ad ogni iterazione genero un list item
+            const li = document.createElement('li')
+            //ad ogni itearazione genero un list item valorizzato
+            li.textContent = email
+            //appendo il list item valorizzato nel tag ul
+            emailList.appendChild(li)
         })
         .catch(() => {
 
